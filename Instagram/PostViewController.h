263bc699501d1ee2b9c19aa560a9b15c349f5c6c
smwright1 <59848745+smwright1@users.noreply.github.com>
2021-07-08 18:15:03 -0400

    implemented posting photos functionality

---
+++ Instagram/PostViewController.h
@@ -0,0 +1,16 @@
+//
+//  PostViewController.h
+//  Instagram
+//
+//  Created by Storm Wright on 7/8/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PostViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/PostViewController.m
@@ -0,0 +1,85 @@
+//
+//  PostViewController.m
+//  Instagram
+//
+//  Created by Storm Wright on 7/8/21.
+//
+
+#import "PostViewController.h"
+#import "Post.h"
+
+@interface PostViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+@property (weak, nonatomic) IBOutlet UITextField *captionView;
+
+@end
+
+@implementation PostViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    UIImagePickerController *imagePickerVC = [UIImagePickerController new];
+    imagePickerVC.delegate = self;
+    imagePickerVC.allowsEditing = YES;
+
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else {
+        NSLog(@"Camera 🚫 available so we will use photo library instead");
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+
+    [self presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    // Get the image captured by the UIImagePickerController
+    UIImage *originalImage = info[UIImagePickerControllerOriginalImage];
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+
+    self.photoView.image = originalImage;
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+- (IBAction)post:(id)sender {
+    UIImage *image = [self resizeImage:self.photoView.image withSize:CGSizeMake(390, 390)];
+    
+    [Post postUserImage:image withCaption:self.captionView.text withCompletion:^(BOOL succeeded, NSError *error) {
+        if (succeeded) {
+            NSLog(@"Successfuly posted image");
+            [self dismissViewControllerAnimated:true completion:nil];
+        } else {
+            NSLog(@"error %@", error);
+        }
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
